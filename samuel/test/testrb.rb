66758af01258d3c4e2f9845f705d707b3e67f6ff
H. R. Samuel Kazempour Mofrad <samuel@unus.one>
2016-12-04 16:54:39 +0100

    Add Test Folder

---
+++ samuel/test/testrb.rb
@@ -0,0 +1,13 @@
+# Sag Hallo zu allen
+def sag_hallo
+  if @names.nil?
+    puts "..."
+  elsif @names.respond_to?("each")
+    # @names ist eine Liste, iteriere!
+    @names.each do |name|
+      puts "Hallo, #{name}!"
+    end
+  else
+    puts "Hallo, #{@names}!"
+  end
+end
